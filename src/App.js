72a71ef509538b1950f2d43e84db011e0c12f9cf
Teja Nayake <prasadnaik1092@gamil.com>
2023-07-29 20:12:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useEffect , useState} from "react";
+import PokemonThumbnail from "./Components/PokemonThumbnail";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+  const [allPokemons,setAllPokemons] = useState([]);
+  const [loadPoke,setLoadPoke] = useState('https://pokeapi.co/api/v2/pokemon?limit=20');
+  const getAllPokemons = async () => {
+    const res = await fetch(loadPoke)
+    const data = await res.json()
+    setLoadPoke(data.next)
+
+    function createPokemonObject(result){
+      result.forEach(async (pokemon) => {
+        const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+        const data = await res.json();
+        setAllPokemons(currentList => [...currentList,data])
+      });
+    }
+    createPokemonObject(data.results)
+    await console.log(allPokemons)
+  }
+    useEffect(() => {
+      getAllPokemons()
+    },[])
+
+    return (
+      <div className="app-container">
+        <h1>Pokemon Kingdom .</h1>
+
+        <div className="pokemon-container">
+          <div className="all-container">
+            {allPokemons.map((pokemon,index)=>
+            <PokemonThumbnail 
+              id={pokemon.id}
+            
+            />
+            )}
+          </div>
+        </div>
+      </div>
+    )
 }
 
 export default App;
